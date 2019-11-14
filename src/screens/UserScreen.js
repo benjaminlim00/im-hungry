04a72b11895feb6d5efe1983c8a5e1de91c12604
benjamin <benjaminlim1996@hotmail.sg>
2019-11-14 16:05:20 +0900

    added profile page

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,16 +1,90 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import AppBar from '../components/ToolBar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-const UserScreen = () => {
-  return (
-    <View>
-      <AppBar />
-      <Text>User</Text>
-    </View>
-  );
-};
+export default class UserScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}></View>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }}
+        />
+        <View style={styles.body}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.name}>John Doe</Text>
+            <Text style={styles.info}>Korea, seoul</Text>
+            <Text style={styles.description}>
+              Hungry boy, trying to lose weight, please show me some support
+            </Text>
 
-const styles = StyleSheet.create({});
+            {/* <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Opcion 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Opcion 2</Text>
+            </TouchableOpacity> */}
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 
-export default UserScreen;
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#008b50',
+    height: 200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 130,
+  },
+  name: {
+    fontSize: 22,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: '600',
+  },
+  info: {
+    fontSize: 16,
+    color: '#00BFFF',
+    marginTop: 8,
+  },
+  description: {
+    fontSize: 16,
+    color: '#696969',
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#008b50',
+  },
+});
